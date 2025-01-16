3bc7948e2296921a4b362c273763b9d456c29cfa
Moyses Emanuel <164270978+moysesemanuel@users.noreply.github.com>
2025-01-16 17:32:47 -0300

    #

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class Pessoa {
+   private String nome;
+   private String cpf;
+   private  String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
     }
 }
